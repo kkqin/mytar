e6b2cda5d4357b7096ec53123ee008b9ca821181
k <xolaoqin@gmail.com>
2020-09-25 17:15:30 +0800

    tartar

--- mytar.c
+++ mytar.c
@@ -1,47 +1,5 @@
 #include "mytar.h"
 
-#define  lf_oldnormal '\0'       /* normal disk file, unix compatible */
-#define  lf_normal    '0'        /* normal disk file */
-#define  lf_link      '1'        /* link to previously dumped file */
-#define  lf_symlink   '2'        /* symbolic link */
-#define  lf_chr       '3'        /* character special file */
-#define  lf_blk       '4'        /* block special file */
-#define  lf_dir       '5'        /* directory */
-#define  lf_fifo      '6'        /* fifo special file */
-#define  lf_contig    '7'        /* contiguous file */
-
-enum {
-	 HEAD = 1,
-	 BODY 
-};
-
-#pragma pack(1) 
-struct _tar_head{
-	union {
-		struct {
-			char name[100];	
-			char mode[8];
-			char uid[8];
-			char gid[8];
-			char size[12];
-			char mtime[12];
-			char chksum[8];
-			char type;
-			char link_name[100];
-			char ustar[8];
-			char owner[32];
-			char group[32];
-			char major[32];
-			char minor[32];
-		};
-		char block[512];
-	};
-
-	int itype;
-	int end;
-	struct _tar_head* next;
-};
-
 unsigned int oct2uint(const char* src, int read_size) {
 	unsigned int result = 0; // 下标从0开始	
 	int i = 0;
--- mytar.h
+++ mytar.h
@@ -21,7 +21,48 @@
 
 #include <string.h>
 
-struct _tar_head;
+#define  lf_oldnormal '\0'       /* normal disk file, unix compatible */
+#define  lf_normal    '0'        /* normal disk file */
+#define  lf_link      '1'        /* link to previously dumped file */
+#define  lf_symlink   '2'        /* symbolic link */
+#define  lf_chr       '3'        /* character special file */
+#define  lf_blk       '4'        /* block special file */
+#define  lf_dir       '5'        /* directory */
+#define  lf_fifo      '6'        /* fifo special file */
+#define  lf_contig    '7'        /* contiguous file */
+
+enum {
+	 HEAD = 1,
+	 BODY 
+};
+
+#pragma pack(1) 
+struct _tar_head{
+	union {
+		struct {
+			char name[100];	
+			char mode[8];
+			char uid[8];
+			char gid[8];
+			char size[12];
+			char mtime[12];
+			char chksum[8];
+			char type;
+			char link_name[100];
+			char ustar[8];
+			char owner[32];
+			char group[32];
+			char major[32];
+			char minor[32];
+		};
+		char block[512];
+	};
+
+	int itype;
+	int end;
+	struct _tar_head* next;
+};
+
 // 8 进制转 10进制
 unsigned int oct2uint(const char* src, int read_size);
 // 文件存在
